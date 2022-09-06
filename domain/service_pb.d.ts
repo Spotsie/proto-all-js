163c4b3be5f7a9628c9376700d26f28c781d0da2
protodist <email@example.com>
2022-09-06 09:21:58 +0000

    add pb files

---
+++ domain/service_pb.d.ts
@@ -0,0 +1,9 @@
+// package: domain
+// file: domain/service.proto
+
+import * as jspb from "google-protobuf";
+import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
+import * as google_protobuf_duration_pb from "google-protobuf/google/protobuf/duration_pb";
+import * as google_protobuf_struct_pb from "google-protobuf/google/protobuf/struct_pb";
+import * as domain_domain_pb from "../domain/domain_pb";
+
